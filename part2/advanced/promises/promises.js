f5496e09b315fd3146f3bd08d2527a53d394bedb
mounishvatti <mounishvatti2002@gmail.com>
2024-08-04 22:48:33 +0530

    added lexical scoping concept in javascript

--- part2/advanced/promises/promises.js
+++ part2/advanced/promises/promises.js
@@ -43,80 +43,77 @@ const promiseFour = new Promise(function (resolve, reject) {
   }, 1000);
 });
 
-promiseFour.then((user) => {
-  console.log(user);
-  return user.username;
-})
-.then((username) => { // chained promises
-  console.log(username);
-})
-.catch((error) => {
-  console.log(error);
-})
-.finally(() => {
-  console.log("Promise 4 is either resolved or rejected");
-});
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    // chained promises
+    console.log(username);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Promise 4 is either resolved or rejected");
+  });
 
 const promiseFive = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let error = true;
-        if (!error) {
-          resolve({ username: "sourcandy", password: "1234" });
-        } else {
-          reject("ERROR - JS went wrong");
-        }
-    }, 1000);
-})
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "sourcandy", password: "1234" });
+    } else {
+      reject("ERROR - JS went wrong");
+    }
+  }, 1000);
+});
 
 async function consumePromiseFive() {
-    try {
-        const response = await promiseFive
-        console.log(response)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-consumePromiseFive()
+consumePromiseFive();
 
 async function getAllUsers() {
-    try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        console.log("ERROR:",error)
-    }
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
 }
 
-getAllUsers()
+getAllUsers();
 
 // same as above without async/await
 
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log("ERROR:",error))
-
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log("ERROR:", error));
 
 // expanded version of the same code as above
 
 fetch("https://api.github.com/users/mounishvatti")
-    .then( (response) => {
-        return response.json()
-    })
-    .then( (data) => { // chained promises (collects data from the previous return statement)
-        console.log(data)
-    })
-    .catch( (error) => {
-        console.log("ERROR:",error)
-    })
-    
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // chained promises (collects data from the previous return statement)
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("ERROR:", error);
+  });
 
 // Why fetch is processed at the earliest even though it is async?
 
 //Ans: all the fetch calls are sent to the call-back queue (priority queue) hence the order of execution makes sure that the fetch is processed first.
-
-
-
-
